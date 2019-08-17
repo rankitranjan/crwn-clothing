51b43fc4e877ab37a2c4e783603164f3914af2b0
rrankit <rankit.ranjan.contractor@move.com>
2019-08-18 05:19:37 +0530

    implimentd firebnase utils included current user and auth user in firebase db

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -13,6 +13,27 @@ const config = {
     appId: "1:776522122287:web:6958df9bf541737c"
 };
 
+export const creteUserProfileDocument = async (userAuth, additionalData) => {
+	if (!userAuth) return;
+		const userRef = firestore.doc(`users/${userAuth.uid}`);
+		const snapShot = await userRef.get();
+		if (!snapShot.exists) {
+			const { displayName, email } = userAuth;
+			const createdAt = new Date();
+			try	{
+				await userRef.set({
+					displayName,
+					email,
+					createdAt,
+					...additionalData
+				})
+			} catch (error) {
+				console.log('error creating Data', error.message);
+			}
+		}
+	return userRef;
+}
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
