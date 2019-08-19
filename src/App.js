635b34d4c55ad956e6871d4610716e73996f1157
rrankit <rankit.ranjan.contractor@move.com>
2019-08-20 00:36:13 +0530

    add cart icon and cart dropdown and also implimented cart reducer and actions for cart

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import { auth, creteUserProfileDocument } from './firebase/firebase.utils';
-import { setCurrentUser } from './redux/user/user.action';
+import { setCurrentUser } from './redux/user/user.actions';
 
 class App extends React.Component {
   unsubscribeFromAuth = null;
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,11 +4,15 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import { auth } from '../../firebase/firebase.utils';
-import './header.styles.scss';
+
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
 import {ReactComponent as Logo} from  '../../assets/crown.svg'
 
-const Header = ({ currentUser }) => (
+import './header.styles.scss';
+
+const Header = ({ currentUser, hidden }) => (
 	<div className='header'>
 		<Link className='logo-container' to='/'>
 			<Logo className='logo' />
@@ -26,12 +30,17 @@ const Header = ({ currentUser }) => (
 				:
 				<Link className='option' to='/signin'>Sign In</Link>
 			}
+			<CartIcon />
 		</div>
+		{
+			hidden ? null : <CartDropdown />
+		}
 	</div>
 );
 
-const mapStateToProps = state => ({
-	currentUser: state.user.currentUser
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden }}) => ({
+	currentUser,
+	hidden
 });
 
 export default connect(mapStateToProps)(Header);
